93612876b4188b0185b021cdcac69087f914851b
hartmanm <16545449+hartmanm@users.noreply.github.com>
2024-10-13 20:02:52 +0000

    changes to css grid

--- config_resume.js
+++ config_resume.js
@@ -11,7 +11,7 @@ const headline_under_name ='Some <b>Old</b> Projects'
 
 // grid_row_1  a  b  c
 const img_row_1_a  ='<img src="src/d3_road_deaths.png" style="width:100px;height:100px;" alt="src/d3_road_deaths.jp2"/>'
-const name_row_1_a ='d3_road_deaths'
+const name_row_1_a ='road'
 const url_row_1_a  ='https://hartmanm.github.io/d3_road_deaths/d3_road_deaths.html'
 
 const img_row_1_b  ='<img src="src/nvoc.png" style="width:100px;height:100px;" alt="src/nvoc.jp2"/>'
@@ -19,18 +19,18 @@ const name_row_1_b ='nvOC'
 const url_row_1_b  ='https://bitcointalk.org/index.php?topic=1854250.0'
 
 const img_row_1_c  ='<img src="src/lvl3.png" style="width:100px;height:100px;" alt="src/lvl3.jp2"/>'
-const name_row_1_c ='pavo td'
+const name_row_1_c ='pavo'
 const url_row_1_c  ='https://github.com/hartmanm/pavo_td?tab=License-1-ov-file'
 
 // grid_row_2  d  e  f
 const img_row_2_d  ='<img src="src/hs_server.png" style="width:100px;height:100px;" alt="src/hs_server.jp2"/>'
-const name_row_2_d ='hs_server'
+const name_row_2_d ='hs'
 const url_row_2_d  ='https://github.com/hartmanm/hs_server'
 
 const img_row_2_e  ='<img src="src/mining_electric_cost_calculator.png" style="width:100px;height:100px;" alt="src/mining_electric_cost_calculator.jp2"/>'
-const name_row_2_e ='cost calculator'
+const name_row_2_e ='cost'
 const url_row_2_e  ='https://github.com/hartmanm/mining_electric_cost_calculator?tab=License-1-ov-file'
 
 const img_row_2_f  ='<img src="src/link_page.png" style="width:100px;height:100px;" alt="src/link_page.jp2"/>'
-const name_row_2_f ='link_page of more projects'
+const name_row_2_f ='more'
 const url_row_2_f  ='https://hartmanm.github.io/link_page/link_page.html'
