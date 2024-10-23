e4cc04d50cc724b3fccc30d6f84f1c2c2e982a38
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-23 14:29:05 -0700

    simplified config

--- config_resume.js
+++ config_resume.js
@@ -9,32 +9,32 @@ const resume_name         ='Michael Neill Hartman'
 
 const headline_under_name ='Some <b>Old</b> Projects'
 
+const img_css_style       ='style="width:100px;height:100px;"'
+
+const static_src_url      ='https://hartmanm.github.io/static/resume_site/'
+
 // grid_row_1  a  b  c
-const img_row_1_a  ='<img src="https://hartmanm.github.io/static/resume_site/d3_road_deaths.png" style="width:100px;height:100px;"/>'
+const img_row_1_a  ='<img src="'+static_src_url+'d3_road_deaths.png" '+img_css_style+'/>'
 const name_row_1_a ='road'
 const url_row_1_a  ='https://hartmanm.github.io/d3_road_deaths/d3_road_deaths.html'
 
-const img_row_1_b  ='<img src="https://hartmanm.github.io/static/resume_site/nvoc.png" style="width:100px;height:100px;"/>'
+const img_row_1_b  ='<img src="'+static_src_url+'nvoc.png" '+img_css_style+'/>'
 const name_row_1_b ='nvOC'
 const url_row_1_b  ='https://bitcointalk.org/index.php?topic=1854250.0'
 
-const img_row_1_c  ='<img src="https://hartmanm.github.io/static/resume_site/openrig.png" style="width:100px;height:100px;">'
+const img_row_1_c  ='<img src="'+static_src_url+'openrig.png" '+img_css_style+'/>'
 const name_row_1_c ='oapi'
 const url_row_1_c  ='https://github.com/hartmanm/overlord?tab=License-1-ov-file'
 
 // grid_row_2  d  e  f
-const img_row_2_d  ='<img src="https://hartmanm.github.io/static/resume_site/hs_server.png" style="width:100px;height:100px;"/>'
+const img_row_2_d  ='<img src="'+static_src_url+'hs_server.png" '+img_css_style+'/>'
 const name_row_2_d ='hs'
 const url_row_2_d  ='https://github.com/hartmanm/hs_server'
 
-//const img_row_2_e  ='<img src="https://hartmanm.github.io/static/resume_site/mining_electric_cost_calculator.png" style="width:100px;height:100px;"/>'
-//const name_row_2_e ='cost'
-//const url_row_2_e  ='https://github.com/hartmanm/mining_electric_cost_calculator?tab=License-1-ov-file'
-
-const img_row_2_e  ='<img src="https://hartmanm.github.io/static/resume_site/m11.png" style="width:100px;height:100px;"/>'
+const img_row_2_e  ='<img src="'+static_src_url+'m11.png" '+img_css_style+'/>'
 const name_row_2_e ='m11'
 const url_row_2_e  ='https://github.com/hartmanm/m11_iot_trigger_hub?tab=License-1-ov-file'
 
-const img_row_2_f  ='<img src="https://hartmanm.github.io/static/resume_site/link_page.png" style="width:100px;height:100px;"/>'
+const img_row_2_f  ='<img src="'+static_src_url+'link_page.png" '+img_css_style+'/>'
 const name_row_2_f ='more'
 const url_row_2_f  ='https://hartmanm.github.io/link_page/link_page.html'
