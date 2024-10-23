b9e90bcced234425ee061a85cf51743980e9836b
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-23 14:24:16 -0700

    switched to use static img src

--- config_resume.js
+++ config_resume.js
@@ -10,31 +10,31 @@ const resume_name         ='Michael Neill Hartman'
 const headline_under_name ='Some <b>Old</b> Projects'
 
 // grid_row_1  a  b  c
-const img_row_1_a  ='<img src="src/d3_road_deaths.png" style="width:100px;height:100px;"/>'
+const img_row_1_a  ='<img src="https://hartmanm.github.io/static/resume_site/d3_road_deaths.png" style="width:100px;height:100px;"/>'
 const name_row_1_a ='road'
 const url_row_1_a  ='https://hartmanm.github.io/d3_road_deaths/d3_road_deaths.html'
 
-const img_row_1_b  ='<img src="src/nvoc.png" style="width:100px;height:100px;"/>'
+const img_row_1_b  ='<img src="https://hartmanm.github.io/static/resume_site/nvoc.png" style="width:100px;height:100px;"/>'
 const name_row_1_b ='nvOC'
 const url_row_1_b  ='https://bitcointalk.org/index.php?topic=1854250.0'
 
-const img_row_1_c  ='<img src="src/openrig.png" style="width:100px;height:100px;">'
+const img_row_1_c  ='<img src="https://hartmanm.github.io/static/resume_site/openrig.png" style="width:100px;height:100px;">'
 const name_row_1_c ='oapi'
 const url_row_1_c  ='https://github.com/hartmanm/overlord?tab=License-1-ov-file'
 
 // grid_row_2  d  e  f
-const img_row_2_d  ='<img src="src/hs_server.png" style="width:100px;height:100px;"/>'
+const img_row_2_d  ='<img src="https://hartmanm.github.io/static/resume_site/hs_server.png" style="width:100px;height:100px;"/>'
 const name_row_2_d ='hs'
 const url_row_2_d  ='https://github.com/hartmanm/hs_server'
 
-//const img_row_2_e  ='<img src="src/mining_electric_cost_calculator.png" style="width:100px;height:100px;"/>'
+//const img_row_2_e  ='<img src="https://hartmanm.github.io/static/resume_site/mining_electric_cost_calculator.png" style="width:100px;height:100px;"/>'
 //const name_row_2_e ='cost'
 //const url_row_2_e  ='https://github.com/hartmanm/mining_electric_cost_calculator?tab=License-1-ov-file'
 
-const img_row_2_e  ='<img src="src/m11.png" style="width:100px;height:100px;"/>'
+const img_row_2_e  ='<img src="https://hartmanm.github.io/static/resume_site/m11.png" style="width:100px;height:100px;"/>'
 const name_row_2_e ='m11'
 const url_row_2_e  ='https://github.com/hartmanm/m11_iot_trigger_hub?tab=License-1-ov-file'
 
-const img_row_2_f  ='<img src="src/link_page.png" style="width:100px;height:100px;"/>'
+const img_row_2_f  ='<img src="https://hartmanm.github.io/static/resume_site/link_page.png" style="width:100px;height:100px;"/>'
 const name_row_2_f ='more'
 const url_row_2_f  ='https://hartmanm.github.io/link_page/link_page.html'
