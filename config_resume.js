a9fea6526adcbb51935cc48b40958e614f4645c7
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-23 21:13:58 +0000

    removed alt images

--- config_resume.js
+++ config_resume.js
@@ -10,11 +10,11 @@ const resume_name         ='Michael Neill Hartman'
 const headline_under_name ='Some <b>Old</b> Projects'
 
 // grid_row_1  a  b  c
-const img_row_1_a  ='<img src="src/d3_road_deaths.png" style="width:100px;height:100px;" alt="src/d3_road_deaths.jp2"/>'
+const img_row_1_a  ='<img src="src/d3_road_deaths.png" style="width:100px;height:100px;"/>'
 const name_row_1_a ='road'
 const url_row_1_a  ='https://hartmanm.github.io/d3_road_deaths/d3_road_deaths.html'
 
-const img_row_1_b  ='<img src="src/nvoc.png" style="width:100px;height:100px;" alt="src/nvoc.jp2"/>'
+const img_row_1_b  ='<img src="src/nvoc.png" style="width:100px;height:100px;"/>'
 const name_row_1_b ='nvOC'
 const url_row_1_b  ='https://bitcointalk.org/index.php?topic=1854250.0'
 
@@ -23,18 +23,18 @@ const name_row_1_c ='oapi'
 const url_row_1_c  ='https://github.com/hartmanm/overlord?tab=License-1-ov-file'
 
 // grid_row_2  d  e  f
-const img_row_2_d  ='<img src="src/hs_server.png" style="width:100px;height:100px;" alt="src/hs_server.jp2"/>'
+const img_row_2_d  ='<img src="src/hs_server.png" style="width:100px;height:100px;"/>'
 const name_row_2_d ='hs'
 const url_row_2_d  ='https://github.com/hartmanm/hs_server'
 
-//const img_row_2_e  ='<img src="src/mining_electric_cost_calculator.png" style="width:100px;height:100px;" alt="src/mining_electric_cost_calculator.jp2"/>'
+//const img_row_2_e  ='<img src="src/mining_electric_cost_calculator.png" style="width:100px;height:100px;"/>'
 //const name_row_2_e ='cost'
 //const url_row_2_e  ='https://github.com/hartmanm/mining_electric_cost_calculator?tab=License-1-ov-file'
 
-const img_row_2_e  ='<img src="src/m11.png" style="width:100px;height:100px;" alt="src/m11.jp2"/>'
+const img_row_2_e  ='<img src="src/m11.png" style="width:100px;height:100px;"/>'
 const name_row_2_e ='m11'
 const url_row_2_e  ='https://github.com/hartmanm/m11_iot_trigger_hub?tab=License-1-ov-file'
 
-const img_row_2_f  ='<img src="src/link_page.png" style="width:100px;height:100px;" alt="src/link_page.jp2"/>'
+const img_row_2_f  ='<img src="src/link_page.png" style="width:100px;height:100px;"/>'
 const name_row_2_f ='more'
 const url_row_2_f  ='https://hartmanm.github.io/link_page/link_page.html'
